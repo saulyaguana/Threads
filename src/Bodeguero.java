d6ffbd3010ee71994b7da3b8006c8999123b982d
Saúl Yaguana <saulyaguanacruz@gmail.com>
2024-05-24 11:17:29 -0500

    Tareas I, II & II creadas para bodeguero

--- src/Bodeguero.java
+++ src/Bodeguero.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class Bodeguero extends Empleado {
     String local;
+    ArrayList<Cliente> cl1;
 
     public Bodeguero() {}
 
-    public Bodeguero(String local) {
+    public Bodeguero(String local, ArrayList<Cliente> cl1) {
         this.local = local;
+        this.cl1 = cl1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,18 +16,73 @@ public class Main {
 
         // REPARTIDOR
 
-        Estado estado = new Estado(1, true, new Date("2025-01-01"), "Ninguna");
-        Empleado empleado = new Empleado("Praga");
-        Entrega entrega = new Entrega(cl1, new ArrayList<Empleado>((Collection<? extends Empleado>) empleado), "47", new Date("2025-01-01"), "Ninguna");
-        Paquete paquete = new Paquete(estado, entrega, 0, "008", "Paquete de porcelana", 25, 12);
+//        Estado estado = new Estado(1, true, new Date("2025-01-01"), "Ninguna");
+//        Empleado empleado = new Empleado("Praga");
+//        ArrayList<Empleado> empleados = new ArrayList<>();
+//        Entrega entrega = new Entrega(cl1, empleados, "47", new Date(), "Ninguna");
+//        Paquete paquete = new Paquete(estado, entrega, 0, "008", "Paquete de porcelana", 25, 12);
+//
+//        Repartidor repartidor = new Repartidor(43, paquete);
+//
+//        if (repartidor.paquete.estado.estado) {
+//            System.out.println("Paquete entregado exitosamente");
+//        }else {
+//            System.out.println("Paquete faltante");
+//        }
 
-        if (paquete.estado.estado) {
-            System.out.println("Paquete entregado correctamente");
-        }else {
-            System.out.println("Paquete por entregar");
+
+        // BODEGUERO (part I, II && III)
+
+        ArrayList<Cliente> clienteArr = new ArrayList<>();
+
+        Bodeguero bodeguero = new Bodeguero("Local secundario", clienteArr);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Agregar cliente? --> 1 SI --> 0 NO");
+
+        String entrada = sc.nextLine();
+
+        if (entrada.equals("SI")) {
+            System.out.println("Escriba el código del cliente");
+            String codigo_add = sc.nextLine();
+
+            System.out.println("Escriba la calle principal");
+            String calle1_add = sc.nextLine();
+
+            System.out.println("Escriba la calle secundaria");
+            String calle2_add = sc.nextLine();
+
+            System.out.println("Escriba alguna referencia de su ubicación");
+            String ubicacion_add = sc.nextLine();
+
+            System.out.println("Escriba si permanecerá en el lugar el día de la entrega");
+            int estado_add = sc.nextInt();
+
+
+            Direccion dir_add = new Direccion(codigo_add, calle1_add, calle2_add, ubicacion_add, estado_add);
+
+            System.out.println("Escriba el número celular del cliente");
+
+            String numero_add = sc.nextLine();
+
+            Cliente cliente_add = new Cliente(numero_add, dir_add);
+
+            bodeguero.cl1.add(cliente_add);
+
+        } else {
+            System.out.println("No encontrado");
         }
 
-
+        for (var cliente : bodeguero.cl1) {
+            System.out.println("Nuevo cliente agregado");
+            System.out.println();
+            System.out.println(cliente.direccion.codigo);
+            System.out.println(cliente.direccion.calle1);
+            System.out.println(cliente.direccion.calle2);
+            System.out.println(cliente.direccion.referencia);
+            System.out.println(cliente.direccion.actual);
+        }
 
     }
 }
